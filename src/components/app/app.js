01d2d59bbd4d5f2e0620e09f9b6ef05686deda77
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-05-13 06:21:17 +0300

    Шапка страницы

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,9 +1,10 @@
+import AppInfo from '../app-info/app-info';
 import './app.css';
 
 function App() {
     return (
         <div className="app"> 
-
+            <AppInfo/>
         </div>
     );
 }
