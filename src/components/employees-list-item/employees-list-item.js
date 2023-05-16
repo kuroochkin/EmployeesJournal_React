bc8f2f044082bfd8ffe7fb0f08873cd6a7f5d012
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-05-17 00:08:28 +0300

    Поднятие состояний для инициализации премии

--- src/components/employees-list-item/employees-list-item.js
+++ src/components/employees-list-item/employees-list-item.js
@@ -1,32 +1,9 @@
-import { Component } from 'react';
-
 import './employees-list-item.css';
 
-class EmployeesListItem extends Component{
-    constructor(props) {
-        super(props);
-        this.state = {
-            increase: false,
-            promotion: false
-        }
-    }
+const EmployeesListItem = (props) => {
+   
 
-    onIncrease = () => {
-        this.setState(({increase}) => ({
-            increase: !increase
-        }))
-    }
-
-    onPromotion = () => {
-        this.setState(({promotion}) => ({
-            promotion: !promotion
-        }))
-    }
-
-    render() {
-        const {name, salary, onDelete} = this.props;
-        const {increase, promotion} = this.state;
-
+        const {name, salary, onDelete, onToggleIncrease, onToggleRise, increase, promotion} = props;
 
         let classNames="list-group-item d-flex justify-content-between";
         if (increase) {
@@ -39,12 +16,12 @@ class EmployeesListItem extends Component{
     
         return (
             <li className={classNames}>
-                <span className="list-group-item-label" onClick={this.onPromotion}>{name}</span>
+                <span className="list-group-item-label" onClick={onToggleRise}>{name}</span>
                 <input type="text" className="list-group-item-input" defaultValue={salary + '₽'}/>
                 <div className='d-flex justify-content-center align-items-center'>
                     <button type="button"
                         className="btn-cookie btn-sm "
-                        onClick={this.onIncrease}>
+                        onClick={onToggleIncrease}>
                         <i className="fas fa-cookie"></i>
                     </button>
     
@@ -57,7 +34,7 @@ class EmployeesListItem extends Component{
                 </div>
             </li>
         )
-    }
 }
+
 
 export default EmployeesListItem;
