a23c4d9f8d15551e58825a19ddb26217ffb05f52
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-05-16 20:00:07 +0300

    Переписывание App в класс

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,17 +1,25 @@
+import { Component } from 'react';
+
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
 import EmployeesList from '../employees-list/employees-list';
 import EmployeesAddForm from '../employees-add-form/employees-add-form';
+
 import './app.css';
 
-function App() {
+class App extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+             data: [
+                {name: 'Самофеева Анна', salary: 50000, increase: true, id: 1},
+                {name: 'Курочкин Владислав', salary: 100000, increase: false, id: 2},
+            ]
+        }
+    }
 
-    const data = [
-        {name: 'Самофеева Анна', salary: 50000, increase: true, id: 1},
-        {name: 'Курочкин Владислав', salary: 100000, increase: false, id: 2},
-    ];
-
+   render() {
     return (
         <div className="app"> 
             <AppInfo/>
@@ -21,10 +29,15 @@ function App() {
                 <AppFilter/>
             </div>
 
-            <EmployeesList data={data}/>
+            <EmployeesList 
+            data={this.state.data}
+            onDelete={id => console.log(id)}
+            />
             <EmployeesAddForm/>
         </div>
     );
+   }
 }
+
 
 export default App;
